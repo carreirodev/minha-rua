1d63b383924e3f42e295fd0f721fc359ee9aba4d
Eduardo Carreiro <eduardo@wpcwebsolutions.com>
2022-03-26 11:49:16 -0300

    final app

--- js/api.js
+++ js/api.js
@@ -1,4 +1,8 @@
 const inputCep = document.querySelector("#cep");
+const inputRua = document.querySelector("#rua");
+const inputCidade = document.querySelector("#cidade");
+const inputBairro = document.querySelector("#bairro");
+const inputUF = document.querySelector("#UF");
 
 const BASE_URL = "https://brasilapi.com.br/api";
 
@@ -7,9 +11,18 @@ inputCep.onkeyup = async (evento) => {
 	if (inputCep.value.length < 8) {
 		return;
 	}
+
 	//fazendo uma requisição api BRASILAPI para buscar as informações com o cep digitado
 	const resposta = await fetch(`${BASE_URL}/cep/v1/${inputCep.value}`, { method: "GET" });
-	console.log(resposta);
 
-	alert("cep completo");
+	//extraindo json da resposta
+	const conteudoResposta = await resposta.json();
+	inputRua.value = conteudoResposta.street;
+	inputBairro.value = conteudoResposta.neighborhood;
+	inputUF.value = conteudoResposta.state;
+	inputCidade.value = conteudoResposta.city;
+
+	console.log(conteudoResposta);
+
+	// alert("cep completo " + inputCep.value);
 };
